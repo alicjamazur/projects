78b7724ff499d959543811fe406a68236c4d87d5
Alicja <mazur.alicja4@gmail.com>
2020-08-12 18:47:34 -0300

    add web app

---
+++ app-object-detection/web-app/src/config.js
@@ -0,0 +1,9 @@
+export default {
+    IMAGE_FORMAT: ".png",
+
+    apiGateway: {
+      REGION: "us-east-1",
+      URL: "https://ogz49gve3m.execute-api.us-east-1.amazonaws.com/default/simple-test"
+    },
+    
+  };
