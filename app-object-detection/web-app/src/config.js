b7a279e542daf8089dfdb935cfeb77500d877af3
Alicja <mazur.alicja4@gmail.com>
2020-08-27 13:53:39 -0300

    update files

--- app-object-detection/web-app/src/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    IMAGE_FORMAT: ".png",
-
-    apiGateway: {
-      REGION: "us-east-1",
-      URL: "https://ogz49gve3m.execute-api.us-east-1.amazonaws.com/default/simple-test"
-    },
-    
-  };
